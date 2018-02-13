90df94ad2884c40c9df68cdfd3583fa696ef4f6e
Pratted <epratt@mymail.lccc.edu>
2018-02-13 17:30:48 -0500

    Fixed table layout to support columns with weights.

--- app/src/main/java/com/example/edp19/calchulator/HomeActivity.java
+++ app/src/main/java/com/example/edp19/calchulator/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.edp19.calchulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -10,23 +11,31 @@ import android.widget.TextView;
 public class HomeActivity extends AppCompatActivity {
 
     TableLayout table;
+    TableRow headerRow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //initialize widgets on screen
         table = findViewById(R.id.tlGridTable);
+        headerRow = findViewById(R.id.headerRow);
+    }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        addTableHeaders();
 
         for(int i = 0; i < 5; i++){
             TableRow tr = new TableRow(this);
-
             TextView tv1 = new TextView(this);
             TextView tv2 = new TextView(this);
 
-            tv1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f));
-            tv2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f));
+            tv1.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
+            tv2.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 2f));
 
             tv1.setText("Eric");
             tv2.setText("Pratt");
@@ -36,22 +45,17 @@ public class HomeActivity extends AppCompatActivity {
 
             table.addView(tr);
         }
-
-        //addTableHeaders();
     }
 
-
-    public void addTableHeaders(View v){
-
-        TableRow headerRow = new TableRow(this);
+    public void addTableHeaders(){
 
         TextView tvItem = new TextView(this);
         TextView tvBuy = new TextView(this);
         TextView tvHighAlch = new TextView(this);
 
-        tvItem.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1f));
-        tvBuy.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1f));
-        tvHighAlch.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1f));
+        tvItem.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 2f));
+        tvBuy.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
+        tvHighAlch.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
 
         tvItem.setText("Item");
         tvBuy.setText("Buy");
@@ -60,8 +64,6 @@ public class HomeActivity extends AppCompatActivity {
         headerRow.addView(tvItem);
         headerRow.addView(tvBuy);
         headerRow.addView(tvHighAlch);
-
-        table.addView(headerRow);
     }
 
 }
