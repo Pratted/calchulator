bfffc670d628cb3c050dde61ce5fc7bb0941981d
Aditya Khandkar <ask5430@cray.cs.hbg.psu.edu>
2018-04-21 16:12:55 -0400

    Textviews changed to buttons in the SettingsActivity.

--- app/src/main/java/com/example/edp19/calchulator/SettingsActivity.java
+++ app/src/main/java/com/example/edp19/calchulator/SettingsActivity.java
@@ -1,19 +1,19 @@
 package com.example.edp19.calchulator;
 
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class SettingsActivity extends AppCompatActivity  {
-    Button buttonRemoveFavs;
+    Button btnHiddenItems;
+    Button btnBlockedItems;
+    Button btnRemoveFavs;
+    Button btnRestoreDefaults;
     HashMap<Integer, OsrsItem> osrsItems;
     SQLiteDatabase db;
 
@@ -21,18 +21,33 @@ public class SettingsActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        //buttonRemoveFavs = findViewById(R.id.buttonRemoveFavs);
+        //btnRemoveFavs = findViewById(R.id.btnRemoveFavs);
 
-        System.out.println("ABout to unpakc");
-        osrsItems = (HashMap<Integer, OsrsItem> ) getIntent().getSerializableExtra("osrsItems");
+        System.out.println("About to unpack");
+        osrsItems = (HashMap<Integer, OsrsItem>) getIntent().getSerializableExtra("osrsItems");
 
         OsrsDB.getInstance(this).getWritableDatabase(new OsrsDB.OnDBReadyListener() {
             @Override
             public void onDBReady(SQLiteDatabase db) {
                 SettingsActivity.this.db = db;
-
             }
         });
+
+        Cursor c = db.rawQuery("select * from Item", null);
+
+        while(c.moveToNext()) {
+
+        }
+
+        btnHiddenItems = findViewById(R.id.btnHiddenItems);
+        btnBlockedItems = findViewById(R.id.btnBlockedItems);
+        findViewById(R.id.btnRemoveFavs).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeAllFavorites();
+            }
+        });
+        btnRestoreDefaults = findViewById(R.id.btnRestoreDefaults);
     }
 
 
@@ -46,7 +61,7 @@ public class SettingsActivity extends AppCompatActivity  {
             public void onDBReady(SQLiteDatabase db) {
                 SettingsActivity.this.db = db;
 
-                buttonRemoveFavs.setOnClickListener(new View.OnClickListener() {
+                btnRemoveFavs.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
@@ -76,13 +91,10 @@ public class SettingsActivity extends AppCompatActivity  {
         */
     }
 
-    /*
     public void removeAllFavorites() {
         for (Integer id : osrsItems.keySet()) {
             osrsItems.get(id).setFavorite(false);
         }
-
-        SettingsActivity.this.db.execSQL("update Item set isFavorite = 0;");
+        this.db.execSQL("update Item set isFavorite = 0;");
     }
-    */
 }
